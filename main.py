e6175b047823b1b06356ac64922747acba159ece
Prajwal Prashanth <prjwl028@gmail.com>
2021-02-23 23:04:46 +0000

    1.2.0
    Added new commands such as clear, 8ball, and changed ping.

--- main.py
+++ main.py
@@ -69,6 +69,16 @@ async def on_message(message):
     await client.process_commands(message)
 
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server!')
+
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
+
+
 @client.command()
 async def inspire(ctx):
     quote = await get_quote()
@@ -131,6 +141,11 @@ async def new(ctx, arg):
 
 
 @client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount + 1)
+
+
+@client.command()
 async def hello(ctx, arg):
     if arg.lower() == "there":
         await ctx.reply("General Kenobi!")
@@ -151,7 +166,21 @@ async def say(ctx, arg):
 
 @client.command()
 async def ping(ctx):
-    await ctx.reply('Pong!')
+    await ctx.reply(f'Pong! {round(client.latency * 1000)}ms')
+
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = [
+        "It is certain.", "It is decidedly so.", "Without a doubt.",
+        "Yes - definitely.", "You may rely on it.", "As I see it, yes.",
+        "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
+        "Reply hazy, try again.", "Ask again later.",
+        "Better not tell you now.", "Cannot predict now.",
+        "Concentrate and ask again.", "Don't count on it.", "My reply is no.",
+        "My sources say no.", "Outlook not so good.", "Very doubtful."
+    ]
+    await ctx.reply(f'{random.choice(responses)}')
 
 
 @client.command(pass_context=True)
