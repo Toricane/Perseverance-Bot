444b64349f1b02b188243292b70fb6fd84712931
Prajwal Prashanth <prjwl028@gmail.com>
2021-02-25 00:02:15 +0000

    1.2.6
    Changed it so that the status is auto updated without ping

--- main.py
+++ main.py
@@ -61,6 +61,7 @@ async def get_quote():
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity=discord.Game(name='$help'))
 
 
 @client.event
@@ -182,7 +183,6 @@ async def say(ctx, arg):
 @client.command()
 async def ping(ctx):
     await ctx.reply(f'Pong! {round(client.latency * 1000)}ms')
-    await client.change_presence(activity=discord.Game(name='$help'))
 
 
 @client.command(aliases=['8ball'])
