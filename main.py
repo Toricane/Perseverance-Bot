4241fcfa47d30bc8625328b3d59854ade50bdac6
Prajwal Prashanth <prjwl028@gmail.com>
2021-02-27 20:27:21 +0000

    2.0.3
    Added descriptions to each slash command

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ logging.basicConfig(level=logging.INFO)
 
 client = discord.Client(intents=discord.Intents.all())
 slash = SlashCommand(client, sync_commands=True)
-status = cycle(['/help', 'your messages', '/help', 'Never Gonna Give You Up', '/help', 'try /setup if the slash commands are not working'])
+status = cycle([
+    '/help', 'your messages', '/help', 'Never Gonna Give You Up', '/help',
+    'try /setup if the slash commands are not working'
+])
 
 #db["id"] = [788578597488427008, 764683397528158259]
 guild_ids = db["id"]
@@ -96,28 +99,34 @@ async def on_message(message):
         if any(word in msg for word in sad_words):
             if "!" not in msg and "." not in msg and "not" not in msg and "n't" not in msg and "aint" not in msg and "never" not in msg:
                 await message.reply(random.choice(options))
-    
+
     if msg == "/setup":
-      ido = message.guild.id
-      if ido not in db["id"]:
-        ids = db["id"]
-        ids.append(ido)
-        db["id"] = ids
-        print(db["id"])
-        await message.reply("Server has been set up! The bot is restarting! If the error persists, contact Toricane#6391 in Discord to restart the bot!")
-        slash = SlashCommand(client, sync_commands=True)
-        os.execl(sys.executable, sys.executable, *sys.argv)
-      else:
-        await message.reply("Server already setup! The bot is restarting! If the error persists, contact Toricane#6391 in Discord to restart the bot!")
-        slash = SlashCommand(client, sync_commands=True)
-        os.execl(sys.executable, sys.executable, *sys.argv)
+        ido = message.guild.id
+        if ido not in db["id"]:
+            ids = db["id"]
+            ids.append(ido)
+            db["id"] = ids
+            print(db["id"])
+            await message.reply(
+                "Server has been set up! The bot is restarting! If the error persists, contact Toricane#6391 in Discord to restart the bot!"
+            )
+            slash = SlashCommand(client, sync_commands=True)
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        else:
+            await message.reply(
+                "Server already setup! The bot is restarting! If the error persists, contact Toricane#6391 in Discord to restart the bot!"
+            )
+            slash = SlashCommand(client, sync_commands=True)
+            os.execl(sys.executable, sys.executable, *sys.argv)
 
 
 @client.event
 async def on_guild_join(guild):
     for channel in guild.text_channels:
         if channel.permissions_for(guild.me).send_messages:
-            await channel.send('Thank you for inviting me! Try /setup if the commands still don\'t work after 5 minutes.')
+            await channel.send(
+                'Thank you for inviting me! Try /setup if the commands still don\'t work after 5 minutes.'
+            )
             ido = int(guild.id)
             ids = db["id"]
             ids.append(ido)
@@ -149,26 +158,26 @@ async def on_member_remove(member):
     print(f'{member} has left a server.')
 
 
-@slash.slash(name="inspire", guild_ids=guild_ids)
+@slash.slash(name="inspire", description="The bot will send a random inspirational quote", guild_ids=guild_ids)
 async def _inspire(ctx):
     quote = await get_quote()
     await ctx.respond()
     await ctx.send(quote)
 
 
-@slash.slash(name="hi", guild_ids=guild_ids)
+@slash.slash(name="hi", description="The bot will say hello to you", guild_ids=guild_ids)
 async def _hi(ctx):
     await ctx.respond()
     await ctx.send('Hello!')
 
 
-@slash.slash(name="bye", guild_ids=guild_ids)
+@slash.slash(name="bye", description="The bot will say bye to you", guild_ids=guild_ids)
 async def _bye(ctx):
     await ctx.respond()
     await ctx.send('Bye!')
 
 
-@slash.slash(name="list", guild_ids=guild_ids)
+@slash.slash(name="list", description="Lists the encouraging messages", guild_ids=guild_ids)
 async def _list(ctx):
     encouragements = []
     if "encouragements" in db.keys():
@@ -177,26 +186,44 @@ async def _list(ctx):
     await ctx.send(encouragements)
 
 
-@slash.slash(name="delete", guild_ids=guild_ids)
-async def _delete(ctx, arg):
+@slash.slash(name="delete",
+             description="Deletes an encouraging message, /list to see",
+             options=[
+                 manage_commands.create_option(
+                     name="index",
+                     description="The encouraging message's position in the list that you want to delete, try /list to see",
+                     option_type=3,
+                     required=True)
+             ],
+             guild_ids=guild_ids)
+async def _delete(ctx, argone):
     encouragements = []
     if "encouragements" in db.keys():
-        index = int(arg) - 1
+        index = int(argone) - 1
         delete_encouragment(index)
         encouragements = db["encouragements"]
     await ctx.respond()
     await ctx.send(encouragements)
 
 
-@slash.slash(name="new", guild_ids=guild_ids)
-async def _new(ctx, *, arg):
-    encouraging_message = arg
+@slash.slash(name="new",
+             description="Add a new encouraging message",
+             options=[
+                 manage_commands.create_option(
+                     name="message",
+                     description="Add it here",
+                     option_type=3,
+                     required=True)
+             ],
+             guild_ids=guild_ids)
+async def _new(ctx, argone):
+    encouraging_message = argone
     update_encouragements(encouraging_message)
     await ctx.respond()
     await ctx.send(f'New encouraging message added: {encouraging_message}')
 
 
-@slash.slash(name="clear", guild_ids=guild_ids)
+@slash.slash(name="clear", description="Delete messages", guild_ids=guild_ids)
 @commands.has_permissions(manage_messages=True)
 async def _clear(ctx, amount=5):
     amount = int(amount)
@@ -207,7 +234,7 @@ async def _clear(ctx, amount=5):
     await ctx.channel.purge(limit=2)
 
 
-@slash.slash(name="kick", guild_ids=guild_ids)
+@slash.slash(name="kick", description="Kicks a member", guild_ids=guild_ids)
 @commands.has_permissions(kick_members=True)
 async def _kick(ctx, member: discord.Member, *, reason=None):
     await member.kick(reason=reason)
@@ -215,7 +242,7 @@ async def _kick(ctx, member: discord.Member, *, reason=None):
     await ctx.send(f"Kicked {member} because {reason}.")
 
 
-@slash.slash(name="ban", guild_ids=guild_ids)
+@slash.slash(name="ban", description="Bans a member", guild_ids=guild_ids)
 @commands.has_permissions(ban_members=True)
 async def _ban(ctx, member: discord.Member, *, reason=None):
     await member.ban(reason=reason)
@@ -223,11 +250,20 @@ async def _ban(ctx, member: discord.Member, *, reason=None):
     await ctx.send(f"Banned {member.mention} because {reason}.")
 
 
-@slash.slash(name="unban", guild_ids=guild_ids)
+@slash.slash(name="unban",
+             description="Unbans a member",
+             options=[
+                 manage_commands.create_option(
+                     name="member",
+                     description="Add the member name here",
+                     option_type=3,
+                     required=True)
+             ],
+             guild_ids=guild_ids)
 @commands.has_permissions(ban_members=True)
-async def _unban(ctx, *, member):
+async def _unban(ctx, argone):
     banned_users = await ctx.guild.bans()
-    member_name, member_discriminator = member.split('#')
+    member_name, member_discriminator = argone.split('#')
     for ban_entry in banned_users:
         user = ban_entry.user
         if (user.name, user.discriminator) == (member_name,
@@ -239,21 +275,38 @@ async def _unban(ctx, *, member):
             return
 
 
-@slash.slash(name="hello", guild_ids=guild_ids)
-async def _hello(ctx, *, arg):
-    if arg.lower() == "there":
+@slash.slash(name="hello",
+             description="Say hello to someone",
+             options=[
+                 manage_commands.create_option(
+                     name="name",
+                     description='Put either "there" or the name',
+                     option_type=3,
+                     required=True)
+             ],
+             guild_ids=guild_ids)
+async def _hello(ctx, argone):
+    if argone.lower() == "there":
         await ctx.send("General Kenobi!")
     else:
-        textx = arg.lower()
+        textx = argone.lower()
         texty = textx.capitalize()
         await ctx.respond()
         await ctx.channel.send(f"Hello {texty}!")
         await ctx.message.delete()
 
 
-@slash.slash(name="say", guild_ids=guild_ids)
-async def _say(ctx, *, arg):
-    text = arg
+@slash.slash(name="say",
+             description="Make the bot say anything",
+             options=[
+                 manage_commands.create_option(name="message",
+                                               description="Say your message",
+                                               option_type=3,
+                                               required=True)
+             ],
+             guild_ids=guild_ids)
+async def _say(ctx, argone):
+    text = argone
     await ctx.respond()
     await ctx.channel.send(text)
     await ctx.message.delete()
@@ -307,7 +360,9 @@ async def change_status():
         type=discord.ActivityType.watching, name=next(status)))
 
 
-@slash.slash(name="credits", guild_ids=guild_ids)
+@slash.slash(name="credits",
+             description="Shows the credits",
+             guild_ids=guild_ids)
 async def _credits(ctx):
     embed = discord.Embed(colour=discord.Colour.orange())
     embed.set_author(name='Credits')
@@ -325,14 +380,30 @@ async def _credits(ctx):
         value=
         'https://youtu.be/SPTfmiYiuok\nhttps://youtube.com/playlist?list=PLW3GfRiBCHOhfVoiDZpSz8SM_HybXRPzZ',
         inline=False)
+    embed.add_field(
+        name='Documentations Used:',
+        value=
+        'https://discordpy.readthedocs.io/en/latest/\nhttps://discord-py-slash-command.readthedocs.io/en/latest/',
+        inline=False)
     await ctx.respond()
     await ctx.send(embed=embed)
 
 
-@slash.slash(name="help", guild_ids=guild_ids)
-async def _help(ctx, *, arg=None):
+@slash.slash(
+    name="help",
+    description="Shows all the possible commands and how to use them",
+    options=[
+        manage_commands.create_option(
+            name="command",
+            description=
+            'Will show the specific command that you want to know about, or type "help" for all the commands',
+            option_type=3,
+            required=True)
+    ],
+    guild_ids=guild_ids)
+async def _help(ctx, argone):
     embed = discord.Embed(colour=discord.Colour.orange())
-    arg = str(arg).lower()
+    arg = str(argone).lower()
     if arg == "ping":
         embed.set_author(name='Help for /ping')
         embed.add_field(name='/ping', value='Returns "Pong!"', inline=False)
@@ -358,7 +429,9 @@ async def _help(ctx, *, arg=None):
 
     else:
         embed.set_author(name='Help')
-        embed.add_field(name='/help', value='Shows this message', inline=False)
+        embed.add_field(name='/help help',
+                        value='Shows this message',
+                        inline=False)
         embed.add_field(name='/credits',
                         value='Shows the credits.',
                         inline=False)
@@ -421,7 +494,7 @@ async def _help(ctx, *, arg=None):
         await ctx.send(embed=embed)
 
 
-@slash.slash(name="perseverance", guild_ids=guild_ids)
+@slash.slash(name="perseverance", description="Shows a photo of Perseverance", guild_ids=guild_ids)
 async def _perseverance(ctx):
     await ctx.respond()
     await ctx.send(file=discord.File('perseverance.jpeg'))
